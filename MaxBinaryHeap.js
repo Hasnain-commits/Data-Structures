141201cd33cadf63ae26752e2bcde6f81c550332
HayatH <61989790+Hasnain-commits@users.noreply.github.com>
2022-04-30 13:11:30 -0400

    Update MaxBinaryHeap.js

--- MaxBinaryHeap.js
+++ MaxBinaryHeap.js
@@ -1,62 +1,67 @@
-class MaxBinaryHeap {
-  constructor() {
-    this.values = [];
-  }
-
-  swap(val1, val2) {
-    [this.values[val1], this.values[val2]] = [this.values[val2], this.values[val1]];
-  }
-
-  insert(element) {
-    this.values.push(element);
-    this.bubbleUp();
-  }
-
-  bubbleUp() {
-    let currentIdx = this.values.length - 1; 
-    const current = this.values[currentIdx];
-
-    while (currentIdx > 0) {
-      let parentIdx = Math.floor((currentIdx - 1) / 2);
-      const parent = this.values[parentIdx];
-      if (parent >= current) break;
-      this.swap(parentIdx, currentIdx);
-      currentIdx = parentIdx;
+class MaxHeap {
+    constructor() {
+        this.values = [];
     }
-  }
-
-  extractMax() {
-    const max = this.values[0];
-    const min = this.values.pop();
-
-    if (this.values.length > 0) {
-      this.values[0] = min;
-      this.trickleDown();
+    
+    addMax(value) {
+        this.values.push(value);
+        this.bubbleUp();
     }
-
-    return max;
-  }
-
-  trickleDown() {
-    let currentIdx = 0;
-    const current = this.values[currentIdx];
-    const length = this.values.length;
-
-    while (true) {
-      const leftChildIdx = 2 * currentIdx + 1;
-      const rightChildIdx = 2 * currentIdx + 2;
-      const leftChild = leftChildIdx > length ? null : this.values[leftChildIdx];
-      const rightChild = rightChildIdx > length ? null : this.values[rightChildIdx];
-      let swapIdx = null;
-
-      if (leftChild && leftChild > current) swapIdx = leftChildIdx;
-      if (rightChild && ((!swapIdx && rightChild > current) || (swapIdx && rightChild > leftChild))) swapIdx = rightChildIdx;
-      
-
-      if (!swapIdx) break;
-
-      this.swap(swapIdx, currentIdx);
-      currentIdx = swapIdx;
+    
+    bubbleUp() {
+        let idx = this.values.length - 1;
+        const element = this.values[idx];
+        while(idx > 0){
+            let parentIdx = Math.floor((idx - 1)/2);
+            let parent = this.values[parentIdx];
+            if(element[1] <= parent[1]) break;
+            this.values[parentIdx] = element;
+            this.values[idx] = parent;
+            idx = parentIdx;
+        } 
     }
-  }
-};
+    
+    extractMax() {
+        let max = this.values[0];
+        let min = this.values.pop();
+        
+       if (this.values.length > 0) {
+            this.values[0] = min;
+            this.trickleDown();
+        }
+        
+        return max[0];
+    }
+    
+    trickleDown() {
+  let idx = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+        while(true){
+            let leftChildIdx = 2 * idx + 1;
+            let rightChildIdx = 2 * idx + 2;
+            let leftChild,rightChild;
+            let swap = null;
+
+            if(leftChildIdx < length){
+                leftChild = this.values[leftChildIdx];
+                if(leftChild[1] > element[1]) {
+                    swap = leftChildIdx;
+                }
+            }
+            if(rightChildIdx < length){
+                rightChild = this.values[rightChildIdx];
+                if(
+                    (swap === null && rightChild[1] > element[1]) || 
+                    (swap !== null && rightChild[1] > leftChild[1])
+                ) {
+                   swap = rightChildIdx;
+                }
+            }
+            if(swap === null) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = element;
+            idx = swap;
+        }
+    }
+}
