a9191eca88fc311cd1642fc3abc482c8971892fd
HayatH <61989790+Hasnain-commits@users.noreply.github.com>
2022-05-09 10:58:58 -0400

    Update Priority-Queue.js

--- Priority-Queue.js
+++ Priority-Queue.js
@@ -1,75 +1,72 @@
 class Node {
-  constructor(val, priority) {
-    this.val = val;
-    this.priority = priority;
-  }
+    constructor(val, priority) {
+        this.val = val;
+        this.priority = priority;
+    }
 }
 
 class PriorityQueue {
-  constructor() {
-    this.values = [];
-  }
-
-  swap(val1, val2) {
-    [this.values[val2], this.values[val1]] = [this.values[val1], this.values[val2]];
-  }
-
-  enqueue(val, priority) {
-    let node = new Node(val, priority);
-    this.values.push(node);
-    this.bubbleUp();
-  }
-
-  bubbleUp() {
-    let currentIdx = this.values.length - 1;
-    let current = this.values[currentIdx];
-
-    while (currentIdx > 0) {
-      let parentIdx = Math.floor((currentIdx - 1) / 2);
-      let parent = this.values[parentIdx];
-      if (parent.priority <= current.priority) break;
-      this.swap(parentIdx, currentIdx);
-      currentIdx = parentIdx;
+    constructor() {
+        this.values = []; 
     }
-  }
-    dequeue() {
-      let min = this.values[0];
-      let max = this.values.pop();
-      
-      if (this.values.length > 0) {
-        this.values[0] = max;
-        this.trickleDown();
-      }
-
-      return min;
+    
+    enqueue(val, priority) {
+        const newNode = new Node(val, priority);
+        this.values.push(newNode);
+        this.bubbleUp();
     }
-
-    trickleDown() {
-      let currentIdx = 0;
-      let current = this.values[0];
-      let length = this.values.length;
-
-      while (true) {
-        let leftChildIdx = 2 * currentIdx + 1;
-        let rightChildIdx = 2 * currentIdx + 2;
-        let leftChild = leftChildIdx > length ? null : this.values[leftChildIdx];
-        let rightChild = rightChildIdx > length ? null : this.values[rightChildIdx];
-        let swap = null;
-
-        if (leftChild && leftChild.priority <= current.priority) {
-          swap = leftChildIdx;
-        } 
-
-        if (rightChild && (!swap && rightChild.priority < current.priority) || (swap && rightChild.priority < leftChild.priority)) {
-          swap = leftChildIdx;
+    
+    bubbleUp() {
+        let currentIdx = this.values.length - 1;
+        const current = this.values[currentIdx];
+        
+        while (currentIdx > 0) {
+            let parentIdx = Math.floor((currentIdx - 1) / 2);
+            const parent = this.values[parentIdx];
+            if (parent.priority <= current.priority) break;
+            this.values[currentIdx] = parent;
+            this.values[parentIdx] = current;
+            currentIdx = parentIdx;
         }
-
-        if (!swap) break;
-
-        this.swap(swap, currentIdx);
-        currentIdx = swap;
-      }
-
     }
-  
+    
+    dequeue() {
+        let min = this.values[0];
+        let max = this.values.pop();
+        
+        if (this.values.length > 0) {
+            this.values[0] = max;
+            this.trickleDown();
+        }
+        
+        return min.val;
+    }
+    
+    trickleDown() {
+        let currentIdx = 0;
+        const current = this.values[0];
+        const length = this.values.length;
+        
+        while (true) {
+            let leftChildIdx = 2 * currentIdx + 1;
+            let rightChildIdx = 2 * currentIdx + 2;
+            let leftChild, rightChild;
+            let swap = null;
+            
+            if (leftChildIdx < length) {
+                leftChild = this.values[leftChildIdx];
+                if (leftChild.priority < current.priority) swap = leftChildIdx; 
+            } 
+            
+            if (rightChildIdx < length) {
+                rightChild = this.values[rightChildIdx];
+                if ((swap === null && rightChild.priority < current.priority) || (swap !== null && rightChild.priority < leftChild.priority)) swap = rightChildIdx;
+            }
+                            if (swap === null) break;
+                
+                this.values[currentIdx] = this.values[swap];
+                this.values[swap] = current;
+                currentIdx = swap;
+        }
+    }
 }
